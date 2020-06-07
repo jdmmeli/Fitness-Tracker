f1c6f34339849ee6bb0ccda0a309b6bc695b6636
Jason Meli <jdmmeli@yahoo.com>
2020-06-07 17:21:04 -0400

    models folder

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require("express");
-const mongojs = require("mongojs");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
-const databaseUrl = "workout";
-const collections = ["sessions"];
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const db = mongojs(databaseUrl, collections);
+app.use(express.static("public"));
 
-db.on("error", error => {
-    console.log("Database Error:", error);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
 });
