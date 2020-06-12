93e70add8258adf94e61b02a2dae8804460c9c89
Jason Meli <jdmmeli@yahoo.com>
2020-06-11 22:31:42 -0400

    really final commit

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI,{  
+    useNewUrlParser:true,
+    useFindAndModify:false
+})
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/htmlRoutes.js"));
